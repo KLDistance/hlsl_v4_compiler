4cbb50dbe0013c1ec328906324d0fd46b29942ff
KLDistance <dripysumer@gmail.com>
2019-05-13 02:51:50 +0800

    compile single hlsl file.

---
+++ README.md
@@ -0,0 +1,6 @@
+# hlsl v4 compiler
+**By Yunong Wang (DrippySummer @ KLDistance)**
+<br><br>
+Simple compiler of hlsl shader with model version 4 used in daily development.<br>
+This is an implementation of SlimDX.
+<br><br>
--- hlsl_v4_compiler/Compiler.cs
+++ hlsl_v4_compiler/Compiler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,24 +23,76 @@ namespace hlsl_v4_compiler
     }
     public class Compiler
     {
-        private ID3DDevice device = null;
-        private ID3DSwapChain swapChain = null;
-        private RenderForm pseudoForm = null;
-        
+        private string shaderEntryName = null;
+        private string vsShaderFileSuffix = null;
+        private string psShaderFileSuffix = null;
         public Compiler()
         {
-            this.pseudoForm = new RenderForm("pseudoForm");
-            SwapChainDescription swapChainDescription = new SwapChainDescription()
+            this.shaderEntryName = "main";
+            this.vsShaderFileSuffix = ".vs";
+            this.psShaderFileSuffix = ".ps";
+        }
+        public Compiler(string entryName, string vsSuffix, string psSuffix)
+        {
+            this.shaderEntryName = entryName;
+            this.vsShaderFileSuffix = vsSuffix;
+            this.psShaderFileSuffix = psSuffix;
+        }
+        public int CompileSingleFile(string src_file_path, string des_file_path)
+        {
+            switch(this.CheckShaderType(src_file_path))
             {
-                BufferCount = 1,
-                Usage = Usage.RenderTargetOutput,
-                OutputHandle = this.pseudoForm.Handle,
-                IsWindowed = true,
-                ModeDescription = new ModeDescription(0, 0, new Rational(60, 1), Format.R8G8B8A8_UNorm),
-                SampleDescription = new SampleDescription(1, 0),
-                Flags = SwapChainFlags.AllowModeSwitch,
-                SwapEffect = SwapEffect.Discard
-            };
+                // invalid file type
+                case 0:
+                    break;
+                // vertex shader
+                case 1:
+                    {
+                        ShaderBytecode vertexByteCode = ShaderBytecode.CompileFromFile(src_file_path, shaderEntryName,
+                        "vs_4_0", ShaderFlags.None, EffectFlags.None);
+                        this.WriteCsoFile(vertexByteCode.Data, des_file_path);
+                        vertexByteCode.Dispose();
+                    }
+                    break;
+                case 2:
+                    {
+                        ShaderBytecode pixelByteCode = ShaderBytecode.CompileFromFile(src_file_path, shaderEntryName,
+                        "ps_4_0", ShaderFlags.None, EffectFlags.None);
+                        this.WriteCsoFile(pixelByteCode.Data, des_file_path);
+                        pixelByteCode.Dispose();
+                    }
+                    break;
+            }
+            return 0;
+        }
+        private int CheckShaderType(string src_file_path)
+        {
+            string[] filePathArr = src_file_path.Split('.');
+            if(filePathArr[filePathArr.Length - 1].Equals(this.vsShaderFileSuffix))
+            {
+                // 1 means vertex shader
+                return 1;
+            }
+            else if(filePathArr[filePathArr.Length - 1].Equals(this.psShaderFileSuffix))
+            {
+                // 2 means pixel shader
+                return 2;
+            }
+            else
+            {
+                // nope
+                return 0;
+            }
+        }
+        private int WriteCsoFile(DataStream shaderDataStream, string des_file_path)
+        {
+            FileStream fpShader = new FileStream(String.Concat(des_file_path, ".cso"), 
+                FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            byte[] buffer = new byte[shaderDataStream.Length];
+            shaderDataStream.Write(buffer, 0, buffer.Length);
+            fpShader.Write(buffer, 0, buffer.Length);
+            fpShader.Close();
+            return 0;
         }
     }
 }
