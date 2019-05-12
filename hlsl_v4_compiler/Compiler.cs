cc6e2416bcc53f961439c4e480e7b8847cc9615a
KLDistance <dripysumer@gmail.com>
2019-05-13 02:00:00 +0800

    project initiation.

---
+++ hlsl_v4_compiler/Compiler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using SlimDX;
+using SlimDX.D3DCompiler;
+using SlimDX.Direct3D11;
+using SlimDX.DXGI;
+using SlimDX.Windows;
+using ID3DDevice = SlimDX.Direct3D11.Device;
+using ID3DResource = SlimDX.Direct3D11.Resource;
+using ID3DBuffer = SlimDX.Direct3D11.Buffer;
+using ID3DSwapChain = SlimDX.DXGI.SwapChain;
+
+namespace hlsl_v4_compiler
+{
+    public class Semantics
+    {
+
+    }
+    public class Compiler
+    {
+        private ID3DDevice device = null;
+        private ID3DSwapChain swapChain = null;
+        private RenderForm pseudoForm = null;
+        
+        public Compiler()
+        {
+            this.pseudoForm = new RenderForm("pseudoForm");
+            SwapChainDescription swapChainDescription = new SwapChainDescription()
+            {
+                BufferCount = 1,
+                Usage = Usage.RenderTargetOutput,
+                OutputHandle = this.pseudoForm.Handle,
+                IsWindowed = true,
+                ModeDescription = new ModeDescription(0, 0, new Rational(60, 1), Format.R8G8B8A8_UNorm),
+                SampleDescription = new SampleDescription(1, 0),
+                Flags = SwapChainFlags.AllowModeSwitch,
+                SwapEffect = SwapEffect.Discard
+            };
+        }
+    }
+}
---
+++ hlsl_v4_compiler/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hlsl_v4_compiler
+{
+    class EntryClass
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
